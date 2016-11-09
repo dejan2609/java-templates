cb3c9d2aaf117d722450677cf4cdec11fd162718
abogatikov <alexey.bogatikov@gmail.com>
2016-11-09 22:19:24 +0300

    init commit jax-ws

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### Hello World projects for increase coding skills!
+
+[jax-ws](https://github.com/abogatikov/HelloWorld/jaxws)
---
+++ jaxws/server/src/main/java/com/devalexb/service/WSServer.java
@@ -0,0 +1,8 @@
+package com.devalexb.service;
+
+/**
+ * @author Alex Bogatikov
+ *         Created on 09/11/2016.
+ */
+public class WSServer {
+}
