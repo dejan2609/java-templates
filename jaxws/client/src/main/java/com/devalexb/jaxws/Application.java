37a5f5d260f06e6e896f2abf74c7d2dafd1d29e0
abogatikov <alexey.bogatikov@gmail.com>
2016-11-13 09:55:16 +0300

    jaxws: add logging soap handler

--- jaxws/client/src/main/java/com/devalexb/jaxws/Application.java
+++ jaxws/client/src/main/java/com/devalexb/jaxws/Application.java
@@ -1,11 +1,17 @@
 package com.devalexb.jaxws;
 
+import com.devalexb.jaxws.handler.LogSoapMsgHandler;
 import com.devalexb.jaxws.service.HelloWorld;
 import com.devalexb.jaxws.service.WSService;
 import com.devalexb.jaxws.service.WSService_Service;
 
+import javax.xml.ws.handler.Handler;
+import javax.xml.ws.handler.HandlerResolver;
+import javax.xml.ws.handler.PortInfo;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Alex Bogatikov
@@ -15,6 +21,15 @@ public class Application {
     public static void main(String[] args) throws MalformedURLException {
         URL wsdl = new URL("http://127.0.0.1:8080/WSService?wsdl");
         WSService_Service service = new WSService_Service(wsdl);
+        HandlerResolver handlerResolver = new HandlerResolver() {
+            @Override
+            public List<Handler> getHandlerChain(PortInfo portInfo) {
+                List<Handler> handlerList = new ArrayList<>();
+                handlerList.add(new LogSoapMsgHandler("./build/tmp"));
+                return handlerList;
+            }
+        };
+        service.setHandlerResolver(handlerResolver);
         WSService wsService = service.getWSService();
         HelloWorld hw = wsService.geHelloWorld();
         System.out.println(hw.getHelloWorld());
--- jaxws/client/src/main/java/com/devalexb/jaxws/handler/LogSoapMsgHandler.java
+++ jaxws/client/src/main/java/com/devalexb/jaxws/handler/LogSoapMsgHandler.java
@@ -1,8 +1,76 @@
 package com.devalexb.jaxws.handler;
 
+import org.apache.log4j.Logger;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Set;
+
 /**
  * @author Alex Bogatikov
  *         Created on 13/11/2016.
  */
-public class LogSoapMsgHandler {
+public class LogSoapMsgHandler implements SOAPHandler<SOAPMessageContext> {
+
+    private static final Logger LOGGER = Logger.getLogger(LogSoapMsgHandler.class);
+
+    private String fileNamePrefixRequest;
+    private String fileNamePrefixResponse;
+    private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+
+    public LogSoapMsgHandler(String logDir) {
+        this.fileNamePrefixRequest = logDir + File.separator + "request-";
+        this.fileNamePrefixResponse = logDir + File.separator + "response-";
+    }
+
+    @Override
+    public Set<QName> getHeaders() {
+        return null;
+    }
+
+    @Override
+    public boolean handleMessage(SOAPMessageContext context) {
+        try {
+            //if this is a request, true for outbound messages, false for inbound
+            Boolean isRequest = (Boolean) context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+            String fileNamePostfix = format.format(new Date());
+
+            if (isRequest) {
+                SOAPMessage msg = context.getMessage();
+                writeMessageToFile(msg, new File(fileNamePrefixRequest + fileNamePostfix));
+
+            } else {
+                SOAPMessage msg = context.getMessage();
+                writeMessageToFile(msg, new File(fileNamePrefixResponse + fileNamePostfix));
+            }
+        } catch (Exception ex) {
+            LOGGER.error(ex);
+        }
+        return true;
+    }
+
+    private void writeMessageToFile(SOAPMessage msg, File file) {
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            msg.writeTo(fos);
+        } catch (Exception ex) {
+            LOGGER.error(ex);
+        }
+    }
+
+    @Override
+    public boolean handleFault(SOAPMessageContext soapMessageContext) {
+        return true;
+    }
+
+    @Override
+    public void close(MessageContext messageContext) {
+
+    }
 }
